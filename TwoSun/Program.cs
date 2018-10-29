82aa248ae34f58e8f4bb6f0944ae97e9b824bce7
WGX <634808043@qq.com>
2018-10-29 18:16:27 +0800

    test

---
+++ TwoSun/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSun
+{
+    class Program
+    {
+        public static int[] _nums = { 2, 7, 11, 15 };
+        public static int _target = 9;
+        static void Main(string[] args)
+        {
+            int[] arr=TwoSum(_nums, _target);
+            foreach (var a in arr)
+            {
+                Console.WriteLine($"{a}");
+            }
+            Console.ReadKey();
+        }
+        public static int [] TwoSum(int[] nums,int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i=0;i<nums.Length;i++)
+            {
+                int result = target - nums[i];
+                if(dic.ContainsKey(result))
+                {
+                    return new int[] { dic[result], i };
+                }
+                dic.Add(nums[i], i);
+            }
+            return null;
+        }
+    }
+}
