89b736db4d61b6c222d01b86d95252cdf8a00611
wangguoxing <wanghuoxing@aossci.com>
2019-12-24 14:40:08 +0800

    5

---
+++ BinaryTreeLevelOrderTraversal/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTreeLevelOrderTraversal
+{
+    /*
+     * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+      
+            IList<IList<int>> list = new List<IList<int>>();
+            if (root == null) return list;
+          Queue <TreeNode> queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                int length = queue.Count;
+                List<int> templist = new List<int>();
+                for (int i = 0; i < length; i++)
+                {
+                    TreeNode treeNode = queue.Dequeue();
+                    templist.Add(treeNode.val);
+                    if (treeNode.left != null)
+                        queue.Enqueue(treeNode.left);
+                    if (treeNode.right != null)
+                        queue.Enqueue(treeNode.right);
+                }
+                list.Add(templist);
+            }
+
+            return list;
+        }
+
+        public IList<IList<int>> LevelOrder2(TreeNode root)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            if (root == null) return list;
+            helper(root, 0, list);
+            return list;
+        }
+        public void helper(TreeNode root,int levels, IList<IList<int>> list)
+        {
+            if (list.Count == levels)
+            {
+                list.Add(new List<int>());
+            }
+            list[levels].Add(root.val);
+
+            if (root.left != null)
+            {
+                helper(root.left, levels+1, list);
+            }
+            if (root.right != null)
+            {
+                helper(root.right, levels + 1, list);
+            }
+        }
+    }
+}
---
+++ MaximumDepthofBinaryTree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumDepthofBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    class Program
+    {
+        /*
+         * 给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            else
+            {
+                int L = MaxDepth(root.left);
+                int R = MaxDepth(root.right);
+                return Math.Max(L, R) + 1;
+            }
+        }
+        public int MaxDepth2(TreeNode root)
+        {
+            if (root == null) return 0;
+            Stack<Tuple<TreeNode, int>> stack = new Stack<Tuple<TreeNode, int>>();
+            stack.Push(new Tuple<TreeNode, int>(root, 1));
+            int depth = 0;
+            while (stack.Count>0)
+            {
+                var temp = stack.Pop();
+                depth = Math.Max(depth, temp.Item2);
+                if (temp.Item1.right != null)
+                {
+                    stack.Push(new Tuple<TreeNode, int>(temp.Item1.right, temp.Item2 + 1));
+                }
+                 if (temp.Item1.left != null)
+                {
+                    stack.Push(new Tuple<TreeNode, int>(temp.Item1.left, temp.Item2 + 1));
+                }
+            }
+            return depth;
+        }
+    }
+}
