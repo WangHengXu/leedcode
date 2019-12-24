54876660afc8d9c6c901bb2cb938f1c714e9dbb0
wangguoxing <wanghuoxing@aossci.com>
2019-12-24 11:40:01 +0800

    2

--- ValidateBinarySearchTree/Program.cs
+++ ValidateBinarySearchTree/Program.cs
@@ -6,55 +6,64 @@ using System.Threading.Tasks;
 
 namespace ValidateBinarySearchTree
 {
-   /* 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+    /* 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
 
-假设一个二叉搜索树具有如下特征：
+ 假设一个二叉搜索树具有如下特征：
 
-节点的左子树只包含小于当前节点的数。
-节点的右子树只包含大于当前节点的数。
-所有左子树和右子树自身必须也是二叉搜索树。
-示例 1:
+ 节点的左子树只包含小于当前节点的数。
+ 节点的右子树只包含大于当前节点的数。
+ 所有左子树和右子树自身必须也是二叉搜索树。
+ 示例 1:
 
-输入:
-    2
-   / \
-  1   3
-输出: true
-示例 2:
+ 输入:
+     2
+    / \
+   1   3
+ 输出: true
+ 示例 2:
 
-输入:
-    5
-   / \
-  1   4
-     / \
-    3   6
-输出: false
-解释: 输入为: [5,1,4,null,null,3,6]。
-     根节点的值为 5 ，但是其右子节点值为 4 。*/
-     public class Tree
+ 输入:
+     5
+    / \
+   1   4
+      / \
+     3   6
+ 输出: false
+ 解释: 输入为: [5,1,4,null,null,3,6]。
+      根节点的值为 5 ，但是其右子节点值为 4 。*/
+    public class TreeNode
     {
-        public int data;
-        public Tree leftTree;
-        public Tree RightTree;
-    }
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
     class Program
     {
         static void Main(string[] args)
         {
-            Tree a = new Tree() { data = 1 };
-            Tree b = new Tree() { data = 2 };
-            Tree c = new Tree() { data = 3 };
-
-            Tree d = new Tree() { data = 4 };
-            Tree e = new Tree() { data = 5 };
-            Tree f = new Tree() { data = 6};
-            Tree g = new Tree() { data = 7 };
-            Tree h = new Tree() { data = 8 };
-            Tree i = new Tree() { data = 9};
 
         }
-        public static  bool IsValidBST(Tree root)
+        public static  bool IsValidBST(TreeNode root)
         {
+
+            return Update(root);
+        }
+        public static bool Update(TreeNode root,int? lower=null,int? upper=null)
+        {
+            if (root == null) return true;
+            int val = root.val;
+            if (lower != null && val<=lower)
+            {
+                return false;
+            }
+            if (upper != null && val >=upper)
+            {
+                return false;
+            }
+            if (!(Update(root.right, val, upper))) return false;
+
+            if (!(Update(root.left, lower, val))) return false;
 
             return true;
         }
