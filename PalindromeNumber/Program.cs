bf0c41a713ca01f373c64e7c3f835e8865100401
wangguoxing <wanghuoxing@aossci.com>
2019-12-24 10:49:52 +0800

    1

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PalindromeNumber
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IsPalindrome(11);
+        }
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
---
+++ RegularExpressionMatching/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RegularExpressionMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool isMatch(string s, string p)
+        {
+            if (string.IsNullOrEmpty(p)) return string.IsNullOrEmpty(s);
+            bool firstmatch = (!string.IsNullOrEmpty(s) && (p[0] == s[0]) || p[0] == '.');
+            if (p.Length >= 2 && p[1] == '*')
+            {
+                return (isMatch(s, p.Substring(2)) || (firstmatch && isMatch(s.Substring(1), p)));
+            }
+            else
+            {
+                return firstmatch && isMatch(s.Substring(1), p.Substring(1));
+            }
+        }
+
+     
+    }
+}
