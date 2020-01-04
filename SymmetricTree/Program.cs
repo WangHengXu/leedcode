7039ca6acdef728bb60bef598bff5c5616446f90
wangguoxing <wanghuoxing@aossci.com>
2020-01-04 19:34:44 +0800

    u

---
+++ SymmetricTree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TreeNodeNamespaces;
+
+namespace SymmetricTree
+{
+    /*
+     * 给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+说明:
+
+如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsSymmetric(TreeNode root)
+        {
+          return  IsSymmetric(root, root);
+        }
+        public bool IsSymmetric(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null) return true;
+            if (root1 == null || root2 == null) return false;
+
+            return (root1.val == root2.val) && IsSymmetric(root1.left, root2.right) && IsSymmetric(root1.right, root2.left);
+        }
+
+        public bool IsSymmetric2(TreeNode root)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var t1 = queue.Dequeue();
+                var t2 = queue.Dequeue();
+                if (t1 == null && t2 == null) continue;
+                if (t1 == null || t2 == null) return false;
+                if (t1.val != t2.val) return false;
+                queue.Enqueue(t1.left);
+                queue.Enqueue(t2.right);
+                queue.Enqueue(t1.right);
+                queue.Enqueue(t2.left);
+
+            }
+            return true;
+        }
+
+    }
+}
