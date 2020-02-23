d2cfe5302d0d88b682ed12784c7ddaefbc406148
恒旭 汪 <634808043@qq.com>
2020-02-23 17:58:30 +0800

    1

---
+++ CountNumberswithUniqueDigits/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CountNumberswithUniqueDigits
+{
+    /*
+     * 给定一个非负整数 n，计算各位数字都不同的数字 x 的个数，其中 0 ≤ x < 10n 。
+
+示例:
+
+输入: 2
+输出: 91
+解释: 答案应为除去 11,22,33,44,55,66,77,88,99 外，在 [0,100) 区间内的所有数字。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/count-numbers-with-unique-digits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    internal class Program
+    {
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return 10;
+            int sum = 0 + 1 + 9;
+            for (int i = 2; i <= n; i++)
+            {
+                sum = sum + 9 * JieCheng(i - 1);
+            }
+            return sum;
+        }
+
+        public int JieCheng(int k)
+        {
+            int sum = 1;
+            for (int i = 9; 10 - k <= i; i--)
+            {
+                sum *= i;
+            }
+            return sum;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ReversWords/Program.cs
+++ ReversWords/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+
 /// <summary>
 /// /给定一个字符串，逐个翻转字符串中的每个单词。
 
@@ -14,17 +14,12 @@ using System.Text;
 输入字符串中不会包含前置或尾随的空格
 单词与单词之间永远是以单个空格隔开的
 进阶：使用 O(1) 额外空间复杂度的原地解法。*/
+
 /// </summary>
 namespace ReversWords
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            char[] t = { 't', 'h', 'e',' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e' };
-            ReverseWords(t);
-        }
         public static void ReverseWords(char[] s)
         {
             int ringht = s.Length - 1;
@@ -44,33 +39,19 @@ namespace ReversWords
             }
             temp3.Append(temp2.ToString() + " ");
             temp2.Clear();
-            char[] tempchar= temp3.ToString().ToCharArray();
-            for (int i = 0; i < tempchar.Length-1; i++)
+            char[] v = temp3.ToString().ToCharArray();
+            char[] tempchar = v;
+            for (int i = 0; i < tempchar.Length - 1; i++)
             {
                 s[i] = tempchar[i];
             }
         }
 
-        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        private static void Main(string[] args)
         {
-            IList<int> list = new List<int>();
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-
-            }
-        }
-        List<int> templist = new List<int>();
-       List<List<int>> templist2 = new List<List<int>>();
-        public void Helper(int[] nums,int start)
-        {
-            for (int i = start; i < nums.Length - 1; i++)
-            {
-                
-                templist.Add(nums[i]);
-                List<int> t = new List<int>(templist);
-                templist2.Add(t);
-
-            }
+            Console.WriteLine("Hello World!");
+            char[] t = { 't', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e' };
+            ReverseWords(t);
         }
     }
 }
